d664aadebd88c2be029c26dc67c8b8fc802d7901
Justin <justin.dekai.lee32@gmail.com>
2022-08-04 23:42:37 -0700

    Complete Weather App

---
+++ app.js
@@ -0,0 +1,54 @@
+const { response } = require("express");
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+app.get("/", function(req, res) {
+
+    res.sendFile(__dirname + "/index.html");
+
+
+})
+
+app.post("/", function(req, res) {
+
+
+    const query = req.body.cityName;
+    const apiKey = "bbdfdff78aaf333d66b81602e2a2b520";
+    const unit = "imperial"
+
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&units="+unit+"&appid="+apiKey
+
+    https.get(url, function(response){
+        console.log(response.statusCode);
+
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/"+icon+"@4x.png"
+
+            console.log(weatherData);
+            console.log(temp);
+            console.log(weatherDescription);
+            res.write("<p>Weather is " + weatherDescription + " in " + query + "</p>")
+            res.write("<h1>The temperature in " + query + " is " + temp + " degrees Fahrenheit</h1>");
+            res.write("<img src="+imageURL+">");
+            res.send();
+        })
+    })
+
+})
+
+
+
+app.listen(3000, function() {
+    console.log("Server is running on port 3000.");
+})
